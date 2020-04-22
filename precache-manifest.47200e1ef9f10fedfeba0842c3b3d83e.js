9bef200f93c6a2c2ea97270ad544f1f4999881ff
r-i-c-h <rich@richwerden.com>
2020-04-22 15:17:17 -0700

    Updates

---
+++ precache-manifest.47200e1ef9f10fedfeba0842c3b3d83e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c24c46229a1dc635366b26d5b067e32",
+    "url": "/pogo-types/index.html"
+  },
+  {
+    "revision": "5ce3dc85e46b608f426e",
+    "url": "/pogo-types/static/js/2.9c4f3625.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pogo-types/static/js/2.9c4f3625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9020ff70f34bd8c8fb9e",
+    "url": "/pogo-types/static/js/main.c76e3b9b.chunk.js"
+  },
+  {
+    "revision": "bf9c08dde78a14680680",
+    "url": "/pogo-types/static/js/runtime-main.c56719d1.js"
+  }
+]);
